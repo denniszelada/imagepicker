f33ef85e7d3176ad1ffd4f315085bf23d12313bc
Dennis <denniszelada@gmail.com>
2015-05-20 18:36:38 -0300

    Initial Commit

---
+++ pickimages/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  pickimages
+//
+//  Created by Dennis on 5/20/15.
+//  Copyright (c) 2015 Dennis. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
